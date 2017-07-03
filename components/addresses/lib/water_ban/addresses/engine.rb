0ba5e013b8de1bf26e31ce47eed02adb5201971a
YaroslavO <yaroslavo@softwareplanetgroup.com>
2017-07-04 01:32:28 +0300

    add req libs

--- components/addresses/lib/water_ban/addresses/engine.rb
+++ components/addresses/lib/water_ban/addresses/engine.rb
@@ -1,9 +1,10 @@
+
 module WaterBan
   module Addresses
     class Engine < ::Rails::Engine
       isolate_namespace WaterBan::Addresses
       paths["app/views"] << "app/views/water_ban/addresses"
-    
+
       initializer :append_migrations do |app|
         unless app.root.to_s.match(root.to_s)
           config.paths["db/migrate"].expanded.each do |p|
@@ -17,9 +18,9 @@ module WaterBan
           Rails.configuration.cache_classes ? require(c) : load(c)
         end
       end
-    
+
+      config.autoload_paths += Dir["#{config.root}/lib/**/"]
     end
-  
   end
 end
 
